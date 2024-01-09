5e4015c7c357139efca7cbe17c45630d4d67274f
Soumyajit Chatterjee <78317821+soumyajit-web360@users.noreply.github.com>
2024-01-09 11:33:13 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+});
